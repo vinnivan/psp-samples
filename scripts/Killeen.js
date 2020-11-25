28dbef257465d6d4fd946725c41a7519fe83207f
Yijie Li <yijieli214@gmail.com>
2020-11-25 13:58:23 -1000

    Killeen.js

---
+++ scripts/Killeen.js
@@ -0,0 +1,73 @@
+﻿const { Builder, By, Key, until } = require('selenium-webdriver');
+const HTMLParser = require('node-html-parser');
+
+const Utility = require('./scrape-utilities.js');
+
+
+
+ const scrape = async () => {
+
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    let events = [];
+    let errors = [];
+
+
+    try {
+        await driver.get('https://killeen.legistar.com/Calendar.aspx');
+        let element = await driver.findElement(By.css('.rgMasterTable'));
+        let tableHtml = await element.getAttribute('innerHTML');
+
+        let root = HTMLParser.parse(tableHtml);
+        let rows = root.querySelector("tbody").querySelectorAll('tr');
+        let title;
+        let meetingTime;
+        let date;
+        let link;
+
+        debugger;
+
+        for (let i = 0; i < rows.length; i++) {
+			
+            const row = rows[i];
+			
+			let elements = row.querySelectorAll('td');
+			
+			title = elements[0].querySelector('a').text;
+			
+			date = elements[1].text;
+			meetingTime = elements[3].querySelector('span').text;
+			date = `${date} ${meetingTime}`
+			
+			link = 'https://killeen.legistar.com/' + elements[6].querySelector('a').getAttribute('href');
+			
+            let event = {
+                Name: title,
+                Date: date,
+                AgendaUrl: link
+            };
+
+            events.push(event);
+
+
+        }
+    } catch (err) {
+		console.log(err)
+    } finally {
+        await driver.quit();
+    }
+
+    return {
+        Events: events,
+        Errors: errors
+    };
+
+};
+
+
+
+
+let result = scrape().then(r => {
+
+    console.log(r);
+});
