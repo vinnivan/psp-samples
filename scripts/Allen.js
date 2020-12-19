ed946fccbc4d83574922a3d4064c63bd6d485508
Yijie Li <yijieli214@gmail.com>
2020-12-18 16:05:58 -1000

    Allen.js

---
+++ scripts/Allen.js
@@ -0,0 +1,69 @@
+﻿const { Builder, By, Key, until } = require('selenium-webdriver');
+const HTMLParser = require('node-html-parser');
+
+const Utility = require('./scrape-utilities.js');
+
+
+
+ const scrape = async () => {
+
+    let driver = await new Builder().forBrowser('chrome').build();
+
+    let events = [];
+    let errors = [];
+
+
+    try {
+        await driver.get('https://allen.novusagenda.com/AgendaPublic/');
+        let element = await driver.findElement(By.id('ctl00_ContentPlaceHolder1_SearchAgendasMeetings_radGridMeetings_ctl00'));
+        let tableHtml = await element.getAttribute('innerHTML');
+
+        let root = HTMLParser.parse(tableHtml);
+        let rows = root.querySelector("tbody").querySelectorAll('tr');
+        let title;
+        const meetingTime = '6:00 PM';
+        let date;
+        let link;
+
+        debugger;
+
+        for (let i = 0; i < rows.length; i++) {
+			
+            const row = rows[i].querySelectorAll('td');
+			
+			title = row[1].text;
+			
+			date = `${row[0].text+'20'} ${meetingTime}`;
+			
+			link = 'https://allen.novusagenda.com/AgendaPublic/' + row[4].querySelector('a').getAttribute('href');
+			
+            let event = {
+                Name: title,
+                Date: date,
+                AgendaUrl: link
+            };
+
+            events.push(event);
+
+
+        }
+    } catch (err) {
+		console.log(err)
+    } finally {
+        await driver.quit();
+    }
+
+    return {
+        Events: events,
+        Errors: errors
+    };
+
+};
+
+
+
+
+let result = scrape().then(r => {
+
+    console.log(r);
+});
